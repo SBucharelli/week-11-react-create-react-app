cef6dd36ba2c47575862c5fc94bac00770d2c280
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-08-12 21:17:33 -0400

    Add greeting for visitor

--- src/App.js
+++ src/App.js
@@ -2,59 +2,62 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-// @see https://reactjs.org/docs/rendering-elements.html#updating-the-rendered-element
 class Clock extends React.Component {
+  // `constructor` is part of `class` and React components
+  // always call when an instance of our class is created
+  // aka `construct`d
+  // initialize our stated in our constructor
   constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
+    super(props)
+    // bad code: this.state.displayTime = ''
+    // we must set state to an object
+    this.state = {
+      displayTime: '',
+      visitorName: 'Banjo-Kazoowie'
+    }
   }
 
+  // `componentDidMount` is standard in React - this is where we 
+  // load date or otherwise initialize data
   componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
+    this.timerId = setInterval(() => {
+      // call the `tick`
+      this.tick()
+    }, 1000);
   }
 
+  // custom method as seen on reactjs.org
   tick() {
+    console.log("In tick")
     this.setState({
-      date: new Date()
-    });
+      displayTime: new Date().toLocaleTimeString()
+    })
   }
 
+
+  // `render` is the standard for getting html into our web page
   render() {
+    const displayTime = this.state.displayTime
+    const visitorName = this.state.visitorName
+
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      <div className="clock">
+        <h2>Time is {displayTime}</h2>
+        <dive>Thanks for visiting {visitorName}</dive>
       </div>
-    );
+    )
   }
 }
 
-// @see https://reactjs.org/docs/create-a-new-react-app.html
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Ok.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="" alt="logo" />
       </header>
-      // See https://reactjs.org/docs/rendering-elements.html#updating-the-rendered-element
-      <Clock date={new Date()} />,
+      <Clock />
     </div>
-  );
+  )
 }
 
 export default App;
